e3266880de07ea01fb86d891c7163dce1e3d6499
FIlipe <filipe.cavaleiro2018@gmail.com>
2025-03-27 16:25:56 -0300

    Add files via upload

--- funcionamento.js
+++ funcionamento.js
@@ -5,13 +5,13 @@ function Jogar()
     if ((computador == 0 && jogador == 2) ||
         (computador == 1 && jogador == 0) ||
         (computador == 2 && jogador == 1)) {
-        resultado("<span style='font-size: 20px;'>Sou universitário de Análise e Desenvolvimento de Sistemas, cursando o 3º semestre.<br>Tenho estudado bastante e meu objetivo é me tornar um Dev Full-stack.<br>Para isso, faço diversos cursos na Coursera, estudando Back-end, Front-end e Dados.<br>Sei que é um caminho difícil, mas tenho força de vontade e sei que um dia me tornarei um Dev!<br>Este blog foi criado para que você possa acompanhar minha jornada. Aqui estão alguns dos meus projetos<br><span style='animation: slideIn 1.5s ease-in-out;'>Perdeu!</span></span>", computador);
+        resultado("Perdeu!", computador);
     }
     else if (computador == jogador) {
-        resultado("<span style='font-size: 20px;'>Sou universitário de Análise e Desenvolvimento de Sistemas, cursando o 3º semestre.<br>Tenho estudado bastante e meu objetivo é me tornar um Dev Full-stack.<br>Para isso, faço diversos cursos na Coursera, estudando Back-end, Front-end e Dados.<br>Sei que é um caminho difícil, mas tenho força de vontade e sei que um dia me tornarei um Dev!<br>Este blog foi criado para que você possa acompanhar minha jornada. Aqui estão alguns dos meus projetos<br><span style='animation: slideIn 1.5s ease-in-out;'>Empatou!</span></span>", computador);
+        resultado("Empatou!", computador);
     }
     else {
-        resultado("<span style='font-size: 20px;'>Sou universitário de Análise e Desenvolvimento de Sistemas, cursando o 3º semestre.<br>Tenho estudado bastante e meu objetivo é me tornar um Dev Full-stack.<br>Para isso, faço diversos cursos na Coursera, estudando Back-end, Front-end e Dados.<br>Sei que é um caminho difícil, mas tenho força de vontade e sei que um dia me tornarei um Dev!<br>Este blog foi criado para que você possa acompanhar minha jornada. Aqui estão alguns dos meus projetos<br><span style='animation: slideIn 1.5s ease-in-out;'>Sortudo, você Venceu!</span></span>", computador);
+        resultado("Sortudo, você Venceu!", computador);
     }
 }
 
