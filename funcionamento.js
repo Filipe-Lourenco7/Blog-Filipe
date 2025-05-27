4ec8435241eebbef03b4cc000d9f69a75307dd62
FIlipe <filipe.cavaleiro2018@gmail.com>
2025-05-27 13:44:26 -0300

    Add files via upload

--- funcionamento.js
+++ funcionamento.js
@@ -1,7 +1,20 @@
-function Jogar()
-{
-    const computador = numeroAleatorio(0,2);
+function resultado(mensagem, computador) {
+    const opcoes = ['Pedra 🗿', 'Papel 📄', 'Tesoura ✂️'];
+    const resultadoDiv = document.getElementById("resultadoJogo");
+    resultadoDiv.innerHTML = `
+        <p><strong>Computador escolheu:</strong> ${opcoes[computador]}</p>
+        <p><strong>${mensagem}</strong></p>
+    `;
+}
+
+function numeroAleatorio(min, max) {
+    return (Math.random() * (max - min) + min).toFixed();
+}
+
+function Jogar() {
+    const computador = numeroAleatorio(0, 2);
     const jogador = document.querySelector("select").selectedIndex;
+
     if ((computador == 0 && jogador == 2) ||
         (computador == 1 && jogador == 0) ||
         (computador == 2 && jogador == 1)) {
@@ -13,15 +26,4 @@ function Jogar()
     else {
         resultado("Sortudo, você Venceu!", computador);
     }
-}
-
-function resultado(mensagem, computador) 
-{
-    const opcoes = ['Pedra 🗿', 'Papel 📄', 'Tesoura ✂️'];
-    document.querySelector("h2").innerHTML = mensagem;
-    document.querySelector("p").innerHTML = opcoes[computador];
-}
-
-function numeroAleatorio(min, max) {
-    return (Math.random() * (max - min) + min).toFixed();
 }
